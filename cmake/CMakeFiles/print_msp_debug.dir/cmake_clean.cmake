4955a4f1792c1624c35a1165aa7fc940c834c9a2
JJJJJJJack <hexiang422@gmail.com>
2023-07-24 10:37:36 +0800

    First commit, READING TESTED

---
+++ cmake/CMakeFiles/print_msp_debug.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/print_msp_debug.dir/examples/print_msp_debug.cpp.o"
+  "print_msp_debug"
+  "print_msp_debug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/print_msp_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
